64ad049bd6d98b72f7675e89aa3e56fd1fa66bfe
Dave Kalthoff <dkalthoff@hotmail.com>
2019-06-12 22:33:09 -0500

    Updated to display blocked on one screen and added screen for cached requests

--- piHoleToLCDDisplay.py
+++ piHoleToLCDDisplay.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 import time
 from datetime import date
 import requests
 import RPi.GPIO as GPIO
 from RPLCD import CharLCD
 
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], 
+		numbering_mode=GPIO.BOARD)
+
 
 def writeToLCD( message, lineNumber, sleepSeconds ):
 	if ( lineNumber == 1 ):
@@ -15,30 +19,34 @@ def writeToLCD( message, lineNumber, sleepSeconds ):
 	lcd.write_string(message)
 	time.sleep(sleepSeconds)
 
-	return
+	return;
 
 try:
 	while True:
 		data_pihole1 = requests.get("http://localhost/admin/api.php").json()
-		data_pihole2 = requests.get("http://192.168.1.56/admin/api.php").json()
+		data_pihole2 = requests.get("http://192.168.1.4/admin/api.php").json()
 
 		totalAdsBlocked = data_pihole1['ads_blocked_today'] + data_pihole2['ads_blocked_today']
-		totalPercentageBlocked = (data_pihole1['ads_percentage_today'] + data_pihole2['ads_percentage_today']) / 2
 		totalDNSQueries = data_pihole1['dns_queries_today'] + data_pihole2['dns_queries_today']
+		totalDNSQueriesCached = data_pihole1['queries_cached'] + data_pihole2['queries_cached']
+
+		totalPercentageBlocked = (totalAdsBlocked / totalDNSQueries) * 100
+		totalPercentageCached = (totalDNSQueriesCached / totalDNSQueries) * 100
+
 		fileLastRefreshedDatePiHole1 = date.fromtimestamp(data_pihole1['gravity_last_updated']['absolute'])
 		fileLastRefreshedDatePiHole2 = date.fromtimestamp(data_pihole2['gravity_last_updated']['absolute'])
 
-		writeToLCD("Requests Blocked", 1, 0)
-		writeToLCD("{:,}".format(totalAdsBlocked) + " ({0:.0f}%)".format(totalPercentageBlocked), 2, 10)
+		writeToLCD("Blocked: ({0:.0f}%)".format(totalPercentageBlocked), 1, 0)
+		writeToLCD("{:,}/{:,}".format(totalAdsBlocked, totalDNSQueries), 2, 5)
 
-		writeToLCD("Total Requests", 1, 0)
-		writeToLCD(str(totalDNSQueries), 2, 10)
+		writeToLCD("Cached: ({0:.0f}%)".format(totalPercentageCached), 1, 0)
+		writeToLCD("{:,}/{:,}".format(totalDNSQueriesCached, totalDNSQueries), 2, 5)
 
 		writeToLCD("Pi-Hole1 BlkList", 1, 0)
-		writeToLCD("Update: " + fileLastRefreshedDatePiHole1.strftime("%m/%d/%y"), 2, 5)
+		writeToLCD("Update: " + fileLastRefreshedDatePiHole1.strftime("%m/%d/%y"), 2, 3)
 
 		writeToLCD("Pi-Hole2 BlkList", 1, 0)
-		writeToLCD("Update: " + fileLastRefreshedDatePiHole2.strftime("%m/%d/%y"), 2, 5)
+		writeToLCD("Update: " + fileLastRefreshedDatePiHole2.strftime("%m/%d/%y"), 2, 3)
 
 except KeyboardInterrupt:
 	print("Keyboard interrupt")
